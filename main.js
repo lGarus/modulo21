b3b7e8c062d93095371e421eb2b8696bcb1c505a
lGarus <128258828+lGarus@users.noreply.github.com>
2023-05-23 16:06:06 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+const questions = document.querySelectorAll('[data--faq--question]');
+
+for (let i = 0; i < questions.length; i++) {
+    questions[i].addEventListener('click', abreOuFechaResposta);
+}
+
+function abreOuFechaResposta(elemento) {
+    const classe = 'faq__questions__item--is--open';
+    const elementoPai = elemento.target.parentNode;
+
+    elementoPai.classList.toggle(classe);
+}
